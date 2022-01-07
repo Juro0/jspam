10f58da4df51bf47ac848995936d62463218a83f
Juri <78843415+Juro0@users.noreply.github.com>
2022-01-07 18:22:00 +0100

    Translate EN > IT

--- code.py
+++ code.py
@@ -6,15 +6,15 @@ import time
 #var
 v = 1
 
-#creazione finestra
+# windows
 f = Tk()
 
-#personalizzazione
+# custom win
 f.title("Jspam")
 f.geometry("450x250")
 f.configure(bg="#c0c0c0")
 
-#funzioni
+# def
 def spamma():
     v = int(write2.get())
     f.clipboard_clear()
@@ -24,31 +24,31 @@ def spamma():
         p.hotkey('ctrl','v')
         p.hotkey('enter')
 
-#widget
+# widgets
 testo1 = Label(f, text="JSPAM", background="#c0c0c0", font="Times 11 bold")
 testo1.pack(side=TOP)
 testo1.configure(foreground="black")
 
-testo2 = Label(f, text="testo da spammare:", background="#c0c0c0")
+testo2 = Label(f, text="Text:", background="#c0c0c0")
 testo2.pack(pady=5)
 
-b = Button(f, text="spamma!", command=spamma)
+b = Button(f, text="spam!", command=spamma)
 b.pack(side=BOTTOM)
 b.configure(cursor="hand2")
 
 write = Entry(f)
 write.pack(pady=3)
-write.insert(0, "Scrivi qui...")
+write.insert(0, "Write here...")
 
-testo2 = Label(f, text="Quante volte vuoi spammare:", background="#c0c0c0")
+testo2 = Label(f, text="How many time:", background="#c0c0c0")
 testo2.pack(pady=3)
 
 write2 = Entry(f)
 write2.pack(pady=3)
 write2.insert(0, "3")
 
-testo3 = Label(f, text="Una volta cliccato spamma avrai 5 secondi per posizionare il tuo cursore nel punto in cui vuoi spammare", background="#c0c0c0", font="Times 6 italic")
+testo3 = Label(f, text="Once you click spam you will have 5 seconds to place your cursor where you want to spam", background="#c0c0c0", font="Times 6 italic")
 testo3.pack(side=BOTTOM, pady=3)
 
-#fine finestra
+# mainloop
 f.mainloop()
